b7bcac502183aaad891f1d34e5fb2d206285d0a8
mrseay24 <sidneyeseay@gmail.com>
2019-12-18 20:17:49 -0500

    Add files via upload

---
+++ Lab3.java
@@ -0,0 +1,38 @@
+/*
+ * CSC3420 - Spring 2015
+ * Sidney Seay - sseay5@student.gsu.edu
+ * DateDue: 01-29-2015
+ * Assignment: 3, Lab3 code
+ * File(s): Lab3.java
+ */
+
+//writes string backwards
+
+import java.util.*;
+
+public class Lab3 {
+	
+	
+	
+
+	 public static void main(String args[]){
+		 
+		 System.out.println("Enter a string: "); 
+		 Scanner sc = new Scanner(System.in);
+		 //here is my StringBuilder
+		  StringBuilder sb = new StringBuilder();
+			
+		 
+			
+		
+		 //is it a palindrome
+		 public static boolean isPalindrome(String s);
+		 
+
+	 }
+	//recursive method
+	 public static String writeBackward(String s, int index){
+			System.out.println(writeBackward - 1);
+			
+	 }
+}
